9a331b41f730d8856eba5b6541247e32b99e4057
muhmmdrizky <muhmmdrizky@gmail.com>
2023-10-04 13:59:24 +0700

    first commit

---
+++ 01-PHP-Dasar/18-Switch-Statement.php
@@ -0,0 +1,17 @@
+<?php
+    $nilai = "D";
+
+    switch ($nilai){
+        case "A":
+            echo "Selamat Anda Lulus Dengan Nilai A";
+            break;
+        case "B":
+            echo "Selamat Anda Lulus Dengan Nilai B";
+            break;
+        case "C":
+            echo "Selamat Anda Lulus Dengan Nilai C";
+            break;
+        default:
+            echo "Mungkin Anda Salah Jurusan";
+    }
+?>
---
+++ 01-PHP-Dasar/19-Ternary-Operator.php
@@ -0,0 +1,9 @@
+<?php
+    $gender = "F";
+    $hi = $gender == "M" ? "Hi, Bro!" : "Hi, Nona!";
+    echo $hi . PHP_EOL;
+
+    $value = 100;
+    $status = $value == 100 ? "Good Job!" : "Try Again";
+    echo $status;
+?>
